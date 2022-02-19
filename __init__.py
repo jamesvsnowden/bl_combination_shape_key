9516663769ce3062b747929c8ba8a84b6a22074c
James Snowden <jamesvsnowden@me.com>
2022-02-19 23:37:51 +0100

    docs update

--- __init__.py
+++ __init__.py
@@ -397,6 +397,44 @@ class CombinationShapeKeyDriversRemove(bpy.types.Operator):
         collection.remove(collection.find(shape.name))
         return {'FINISHED'}
 
+class CombinationShapeKeyDriversSolo(bpy.types.Operator):
+    bl_idname = 'combination_shape_key.drivers_solo'
+    bl_label = "Solo Combination Shape Key Drivers"
+    bl_description = "Unmutes shape key drivers, sets their values to 1.0 and selects the combination shape key"
+    bl_options = {'INTERNAL', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context: bpy.types.Context) -> bool:
+        if context.engine in COMPAT_ENGINES:
+            object = context.object
+            if object is not None and object.type in COMPAT_OBJECTS:
+                shape = object.active_shape_key
+                if shape is not None:
+                    key = shape.id_data
+                    return (key.is_property_set("combination_shape_keys")
+                            and shape.name in key.combination_shape_keys
+                            and driver_find(key, f'key_blocks["{shape.name}"].value') is not None)
+        return False
+
+    def execute(self, context: bpy.types.Context) -> typing.Set[str]:
+        shape = context.object.active_shape_key
+        key = shape.id_data
+
+
+        names = [shape.name]
+        for variable in driver_find(key, f'key_blocks["{shape.name}"].value').driver.variables[2:]:
+            names.append(variable.targets[0].data_path[12:-8])
+
+        for item in key.key_blocks[1:]:
+            name = item.name
+            if name in names:
+                item.mute = False
+                item.value = 1.0
+            else:
+                item.mute = True
+
+        return {'FINISHED'}
+
 class CombinationShapeKeyDriverAdd(bpy.types.Operator):
     bl_idname = 'combination_shape_key.driver_add'
     bl_label = "Add Driver"
@@ -702,6 +740,8 @@ class CombinationShapeKeySettings(bpy.types.Panel):
             col.operator(CombinationShapeKeyDriverAdd.bl_idname, icon='ADD', text="")
             col.operator(CombinationShapeKeyDriverRemove.bl_idname, icon='REMOVE', text="")
             col.separator()
+            col.operator(CombinationShapeKeyDriversSolo.bl_idname, icon='SOLO_ON', text="")
+            col.separator()
             col.operator(CombinationShapeKeyDriverMoveUp.bl_idname, icon='TRIA_UP', text="")
             col.operator(CombinationShapeKeyDriverMoveDown.bl_idname, icon='TRIA_DOWN', text="")
         else:
@@ -773,6 +813,7 @@ CLASSES = [
     CombinationShapeKeyNew,
     CombinationShapeKeyDriversSelect,
     CombinationShapeKeyDriversRemove,
+    CombinationShapeKeyDriversSolo,
     CombinationShapeKeyDriverAdd,
     CombinationShapeKeyDriverRemove,
     CombinationShapeKeyDriverMoveUp,
--- docs/index.md
+++ docs/index.md
@@ -4,10 +4,22 @@ A combination shape key is a shape key that is driven by one or more other shape
 you to have more precise control over the way in which shape keys deform the model and are
 particularly useful in facial animation.
 
-For example, you may have shape keys set up for a character's squint and scowl expressions, and
-separately they work well, but when you combine the two, the result doesn't look good. In this
-case you can create a new combination shape key that corrects the expression when the squint and
-scowl expressions are used together.
+For example, let's say you've set up some shape keys for your character's facial expressions. One
+of these shape keys is *satisfaction*, and another *acceptance*.
+
+![Satisfaction Expression Shape Key](media/satisfaction.gif)
+
+![Acceptance Expression Shape Key](media/acceptance.gif)
+
+You're happy with these shape keys in isolation, but when they are used together the combined
+deformation is not so great. In this case the effect is a little too much.
+
+![Satisfaction and Acceptance Expression Shape Keys](media/satisfaction_acceptance.gif)
+
+In this case you can create a new combination shape key that corrects the expression when
+*satisfaction* and *acceptance* shape keys are used together.
+
+![Satisfaction and Acceptance Expression Combination Shape Key](media/satisfaction_acceptance_combination.gif)
 
 Combination shape keys have been available in other software packages for some time and are
 essential to many workflows. This addon brings simple but highly configurable combination shape
@@ -17,5 +29,5 @@ free to share and sell your creations with other users without them needing the 
 It is possible to create and set up combination shape keys in Blender without this addon. In fact
 this addon relies mostly on Blender's native drivers to work. Nevertheless, setting things up
 manually is time-consuming if you are familiar with how combination shape keys work, and confusing
-if you aren't. This addon makes combination shape keys simple and quick to set up and manage so you
-can concentrate on the creative process.
+if you aren't. This addon makes combination shape keys simple and quick to set up, manage and adjust
+so you can concentrate on the creative process.
